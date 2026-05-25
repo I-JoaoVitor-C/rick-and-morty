9688fabaa3930393a815ee314c2ea0c124ec66a2
Eduardo-Luiz-Campos <campos.santos.eduardo@gmail.com>
2026-05-25 08:26:52 -0300

    Servidor funcional

--- server.js
+++ server.js
@@ -24,14 +24,22 @@ app.get('/personagens', (req, res) => {
     });
 });
 
-app.get('/personagens/:id', (req, res) => {
-    
-    buscarPersonagens(personagens => {
-        res.json(personagens)
-    });
+router.get('/:id', (req, res) => {   //Procura por id
+    const id = Number(req.params.id);
+    const personagens = readData();
+    const personagem = consultas.find(a => a.id === id);
+
+    if (!personagem) { //Verifica se existe o ID
+        return res.status(404).json({
+            erro: "consulta não encontrada",
+            mensagem: "Não existe nenhuma consulta com esse Id"
+        });
+    };
+
+    res.json(personagem);
 });
 
 //INICIA SERVIDOR
 app.listen(3000, () =>{
     console.log('Servidor rodando em http://localhost:3000');
-}); //a
+});
